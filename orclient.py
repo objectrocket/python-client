ca1265198b29ab7038024add79bcfe14ec52458d
Anthony Dodd <Dodd.AnthonyJosiah@gmail.com>
2014-02-24 14:41:53 -0600

    Added base code.

---
+++ orclient.py
@@ -0,0 +1,89 @@
+"""
+"""
+import requests
+
+
+# class ORBaseObject(object):
+#     def __init__(self, api_url, user_key, pass_key):
+#         if (not isinstance(api_url, str) or
+#                 not isinstance(user_key, str) or
+#                 not isinstance(pass_key, str)):
+#             raise TypeError('All parameters should be an instance or subclass of str.')
+
+#         if not api_url.strip().endswith('/'):
+#             api_url = api_url.strip() + '/'
+
+#         self._api_url = api_url
+#         self._user_key = user_key
+#         self._pass_key = pass_key
+
+
+class ORClient(object):
+    def __init__(self, api_url, user_key, pass_key):
+        if (not isinstance(api_url, str) or
+                not isinstance(user_key, str) or
+                not isinstance(pass_key, str)):
+            raise self.ORClientException('All parameters should be an instance or subclass of str.')
+
+        if not api_url.strip().endswith('/'):
+            api_url = api_url.strip() + '/'
+
+        # ORClient properties.
+        self._api_url = api_url + 'v2/'
+        self._user_key = user_key
+        self._pass_key = pass_key
+
+        self._instances = None
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @property
+    def instances(self):
+        if self._instances is None:
+            self._instances = Instances(self)
+
+        return self._instances
+
+    @property
+    def pass_key(self):
+        return self._pass_key
+
+    @property
+    def user_key(self):
+        return self._user_key
+
+    class ORClientException(Exception):
+        pass
+
+
+class Instances(object):
+    def __init__(self, client):
+        if not isinstance(client, ORClient):
+            raise self.InstancesException('Parameter "client" must be an instance or subclass of ORClient.')
+
+        self._client = client
+        self._api_instances_url = self.client.api_url + 'instance/'
+
+    @property
+    def api_instances_url(self):
+        return self._api_instances_url
+
+    @property
+    def client(self):
+        return self._client
+
+    def get(self, instance_name=None):
+        if instance_name is not None and not isinstance(instance_name, str):
+            raise self.InstancesException('Parameter "instance_name" must be an instance or subclass of str.')
+
+        url = self.api_instances_url
+        if instance_name is not None:
+            url = self.api_instances_url + instance_name + '/'
+
+        request = requests.get(url, auth=(self.client.user_key, self.client.pass_key))
+        return request.json()
+
+    class InstancesException(Exception):
+        pass
