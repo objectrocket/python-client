ec2443a1b6ef4599cde08965e04a7a633a553250
Anthony Dodd <Dodd.AnthonyJosiah@gmail.com>
2014-08-18 17:36:11 -0500

    Token auth and basic auth now supported.
    Automatic re-authentication is now implemented for clients configured to
    use token authentication.

--- objectrocket/instances.py
+++ objectrocket/instances.py
@@ -1,11 +1,10 @@
-"""Instances layer."""
+"""Instance operations and instances."""
 import datetime
 import json
-import sys
-
-import requests
 import pymongo
+import requests
 
+from objectrocket import auth
 from objectrocket import constants
 from objectrocket import errors
 from objectrocket import operations
@@ -20,27 +19,35 @@ class Instances(operations.BaseOperationsLayer):
 
     def __init__(self, client_instance):
         super(Instances, self).__init__(client_instance=client_instance)
-        self._api_instances_url = self._client.api_url + 'instance/'
 
+    @auth._token_auto_auth
     def compaction(self, instance_name, request_compaction=False):
         """Retrieve a report on, or request compaction for the given instance.
 
         :param str instance_name: The name of the instance to operate upon.
         :param bool request_compaction: A boolean indicating whether or not to request compaction.
         """
-        url = self._api_instances_url + instance_name + '/compaction/'
+        url = self.url + instance_name + '/compaction/'
 
         if request_compaction:
-            response = requests.post(url,
-                                     auth=(self._client.user_key, self._client.pass_key),
-                                     hooks=dict(response=self._verify_auth))
+            response = requests.post(url, **self.client.default_request_kwargs)
         else:
-            response = requests.get(url,
-                                    auth=(self._client.user_key, self._client.pass_key),
-                                    hooks=dict(response=self._verify_auth))
+            response = requests.get(url, **self.client.default_request_kwargs)
 
         return response.json()
 
+    def _concretize_instance_doc(self, instance_doc):
+        """Concretize and instance document into a :py:class:`BaseInstance` subclass."""
+        service = instance_doc.setdefault('service', 'unknown')
+        cls = BaseInstance
+
+        # If service key is a recognized service type, instantiate its respective instance.
+        if service in self._service_class_map:
+            cls = self._service_class_map[service]
+
+        return cls(instance_document=instance_doc, client=self.client)
+
+    @auth._token_auto_auth
     def create(self, name, size, zone, service_type='mongodb', version='2.4.6'):
         """Create an instance.
 
@@ -60,7 +67,7 @@ class Instances(operations.BaseOperationsLayer):
             raise errors.InstancesException('Invalid value for "version". Must be one of "%s".'
                                             % valid_versions)
 
-        url = self._api_instances_url
+        url = self.url
         data = {
             'name': name,
             'size': size,
@@ -72,50 +79,56 @@ class Instances(operations.BaseOperationsLayer):
         # Not passing service type ATM. Probably will soon though.
         data.pop('type')
 
-        response = requests.post(url,
-                                 auth=(self._client.user_key, self._client.pass_key),
-                                 data=json.dumps(data),
-                                 headers={'Content-Type': 'application/json'},
-                                 hooks=dict(response=self._verify_auth))
+        response = requests.post(url, data=json.dumps(data), **self.client.default_request_kwargs)
         return self._return_instance_objects(response)
 
+    @auth._token_auto_auth
     def get(self, instance_name=None):
         """Get details on one or all instances.
 
         :param str instance_name: The name of the instance to retrieve. If ``None``, then retrieve
             a list of all instances which you have access to.
         """
-        url = self._api_instances_url
+        url = self.url
         if instance_name is not None:
             url += instance_name + '/'
 
-        response = requests.get(url,
-                                auth=(self._client.user_key, self._client.pass_key),
-                                hooks=dict(response=self._verify_auth))
+        response = requests.get(url, **self.client.default_request_kwargs)
         return self._return_instance_objects(response)
 
     def _return_instance_objects(self, response):
-        """Translate response data into :py:class:`objectrocket.instances.Instance` objects.
+        """Translate response data into the appropriate :py:class:`BaseInstance` subclass.
 
-        :param object response: An object having a ``json`` method which returns a dict with a key
-            'data'.
+        :param object response: a response object from which JSON data might be extracted.
         """
         # If no JSON object could be decoded, simply return the response.
         try:
             _json = response.json()
+            data = _json.get('data')
+            if data is None:
+                return response
         except ValueError:
             return response
 
-        data = _json.get('data')
-        if data is None:
-            return response
-        elif isinstance(data, dict):
-            return Instance(instance_document=data, client=self._client)
+        # Translate instance documents to instance objects corresponding to their service.
+        if isinstance(data, dict):
+            return self._concretize_instance_doc(instance_doc=data)
         elif isinstance(data, list):
-            return [Instance(instance_document=doc, client=self._client) for doc in data]
+            return [self._concretize_instance_doc(instance_doc=doc) for doc in data]
         else:
             return response
 
+    @property
+    def _service_class_map(self):
+        """A mapping of services to class objects."""
+        service_map = {
+            'mongodb': MongodbInstance,
+            'redis': RedisInstance,
+            'tokumx': TokumxInstance,
+        }
+        return service_map
+
+    @auth._token_auto_auth
     def shards(self, instance_name, add_shard=False):
         """Get a list of shards belonging to the given instance.
 
@@ -123,30 +136,26 @@ class Instances(operations.BaseOperationsLayer):
         :param bool add_shard: A boolean indicating whether to add a new shard to the specified
             instance.
         """
-        url = self._api_instances_url + instance_name + '/shard/'
+        url = self.url + instance_name + '/shard/'
         if add_shard:
-            response = requests.post(url,
-                                     auth=(self._client.user_key, self._client.pass_key),
-                                     hooks=dict(response=self._verify_auth))
+            response = requests.post(url, **self.client.default_request_kwargs)
         else:
-            response = requests.get(url,
-                                    auth=(self._client.user_key, self._client.pass_key),
-                                    hooks=dict(response=self._verify_auth))
+            response = requests.get(url, **self.client.default_request_kwargs)
 
         return response.json()
 
+    @auth._token_auto_auth
     def stepdown_window(self, instance_name):
         """Get information on the instance's stepdown window.
 
         :param str instance_name: The name of the instance to operate upon.
         """
-        url = self._api_instances_url + instance_name + '/stepdown/'
+        url = self.url + instance_name + '/stepdown/'
 
-        response = requests.get(url,
-                                auth=(self._client.user_key, self._client.pass_key),
-                                hooks=dict(response=self._verify_auth))
+        response = requests.get(url, **self.client.default_request_kwargs)
         return response.json()
 
+    @auth._token_auto_auth
     def set_stepdown_window(self, instance_name, start, end,
                             enabled=True, scheduled=True, weekly=True):
         """Set the stepdown window of the given instance.
@@ -170,7 +179,7 @@ class Instances(operations.BaseOperationsLayer):
             raise errors.InstancesException(str(ex) + 'Time strings should be of the following '
                                                       'format: %s' % constants.TIME_FORMAT)
 
-        url = self._api_instances_url + instance_name + '/stepdown/'
+        url = self.url + instance_name + '/stepdown/'
 
         data = {
             'start': start,
@@ -180,46 +189,41 @@ class Instances(operations.BaseOperationsLayer):
             'weekly': weekly,
         }
 
-        response = requests.post(url,
-                                 auth=(self._client.user_key, self._client.pass_key),
-                                 data=json.dumps(data),
-                                 headers={'Content-Type': 'application/json'},
-                                 hooks=dict(response=self._verify_auth))
+        response = requests.post(url, data=json.dumps(data), **self.client.default_request_kwargs)
         return response.json()
 
+    @property
+    def url(self):
+        """The base URL for instance operations."""
+        return self.client.url + 'instance/'
 
-class Instance(object):
-    """The base class of an ObjectRocket service.
+
+class BaseInstance(object):
+    """The base class for ObjectRocket service instances.
 
     :param dict instance_document: A dictionary representing the instance object.
-    :param object client: An instance of :py:class:`objectrocket.client.Client`.
+    :param object client: An instance of :py:class:`objectrocket.client.Client`, most likely coming
+        from the :py:class:`objectrocket.instance.Instances` service layer.
     """
 
     def __init__(self, instance_document, client):
         self._client = client
-        self.instance_document = instance_document
+        self._instance_document = instance_document
 
         # Bind pseudo private attributes from instance_document.
         self._api_endpoint = instance_document['api_endpoint']
         self._connect_string = instance_document['connect_string']
         self._created = instance_document['created']
         self._name = instance_document['name']
-        self._plan = instance_document['plan']
         self._service = instance_document['service']
-        self._ssl_connect_string = instance_document.get('ssl_connect_string')
         self._type = instance_document['type']
         self._version = instance_document['version']
 
-        # Lazily-created properties.
-        self._connection = None
-
-    # -----------------------
-    # DUNDER METHOD OVERLOADS
-    # -----------------------
     def __repr__(self):
+        """Represent this object as a string."""
         _id = hex(id(self))
-        rep = ('<objectrocket.instances.Instance %r at %s>'
-               % (self.instance_document, _id))
+        rep = '<objectrocket.instances.{!s} {!r} at {!s}>'.format(
+            self.__class__.__name__, self.instance_document, _id)
         return rep
 
     @property
@@ -230,18 +234,7 @@ class Instance(object):
     @property
     def client(self):
         """An instance of the objectrocket.client.Client."""
-        if 'objectorcket.client' not in sys.modules:
-            from objectrocket import client
-        if not isinstance(self._client, client.Client):
-            return None
         return self._client
-
-    @property
-    def connection(self):
-        """A live connection to this instance."""
-        if self._connection is None:
-            self._connection = self._get_connection()
-        return self._connection
 
     @property
     def connect_string(self):
@@ -252,6 +245,61 @@ class Instance(object):
     def created(self):
         """The date this instance was created."""
         return self._created
+
+    @property
+    def instance_document(self):
+        """The document used to construct this Instance object."""
+        return self._instance_document
+
+    @property
+    def name(self):
+        """This instance's name."""
+        return self._name
+
+    @property
+    def service(self):
+        """The service this instance provides."""
+        return self._service
+
+    @property
+    def type(self):
+        """The type of service this instance provides."""
+        return self._type
+
+    @property
+    def version(self):
+        """The version of this instance's service."""
+        return self._version
+
+    def to_dict(self):
+        """Render this object as a dictionary."""
+        return self.instance_document
+
+
+class MongodbInstance(BaseInstance):
+    """An ObjectRocket MongoDB service instance.
+
+    :param dict instance_document: A dictionary representing the instance object.
+    :param object client: An instance of :py:class:`objectrocket.client.Client`, most likely coming
+        from the :py:class:`objectrocket.instance.Instances` service layer.
+    """
+
+    def __init__(self, instance_document, client):
+        super(MongodbInstance, self).__init__(instance_document=instance_document, client=client)
+
+        # Bind pseudo private attributes from instance_document.
+        self._plan = instance_document['plan']
+        self._ssl_connect_string = instance_document.get('ssl_connect_string')
+
+        # Lazily-created properties.
+        self._connection = None
+
+    @property
+    def connection(self):
+        """A live connection to this instance."""
+        if self._connection is None:
+            self._connection = self._get_connection()
+        return self._connection
 
     def get_authenticated_connection(self, user, passwd, db='admin'):
         """Establish an authenticated connection to this instance.
@@ -279,42 +327,18 @@ class Instance(object):
             return pymongo.MongoReplicaSetClient(hosts_or_uri=member_list)
 
     @property
-    def name(self):
-        """This instance's name."""
-        return self._name
-
-    @property
     def plan(self):
-        """This instance's plan."""
+        """The base plan size of this instance."""
         return self._plan
-
-    @property
-    def service(self):
-        """The service this instance provides."""
-        return self._service
 
     @property
     def ssl_connect_string(self):
         """This instance's SSL connection string."""
         return self._ssl_connect_string
 
-    def to_dict(self):
-        """Render this object as a dictionary."""
-        return self.instance_document
-
-    @property
-    def type(self):
-        """The type of service this instance provides."""
-        return self._type
-
-    @property
-    def version(self):
-        """The version of this instance's service."""
-        return self._version
-
-    # -------------------
-    # CONVENIENCE METHODS
-    # -------------------
+    #######################
+    # CONVENIENCE METHODS #
+    #######################
     def compaction(self, request_compaction=False):
         """Retrieve a report on, or request compaction for this instance.
 
@@ -338,3 +362,27 @@ class Instance(object):
 
     def set_stepdown_window(self, instance_name, start, end, enabled, scheduled, weekly):
         pass
+
+
+class TokumxInstance(MongodbInstance):
+    """An ObjectRocket TokuMX service instance.
+
+    :param dict instance_document: A dictionary representing the instance object.
+    :param object client: An instance of :py:class:`objectrocket.client.Client`, most likely coming
+        from the :py:class:`objectrocket.instance.Instances` service layer.
+    """
+
+    def __init__(self, instance_document, client):
+        super(TokumxInstance, self).__init__(instance_document=instance_document, client=client)
+
+
+class RedisInstance(BaseInstance):
+    """An ObjectRocket Reids service instance.
+
+    :param dict instance_document: A dictionary representing the instance object.
+    :param object client: An instance of :py:class:`objectrocket.client.Client`, most likely coming
+        from the :py:class:`objectrocket.instance.Instances` service layer.
+    """
+
+    def __init__(self, instance_document, client):
+        super(RedisInstance, self).__init__(instance_document=instance_document, client=client)
--- objectrocket/operations.py
+++ objectrocket/operations.py
@@ -1,27 +1,17 @@
 """Base operations logic for interfacing with various API resources."""
-import sys
-
-from objectrocket import errors
 
 
 class BaseOperationsLayer(object):
-    """A base for operations layer classes; I.E., :py:class:`objectrocket.instances.Instances`."""
+    """A base class for operations layer classes."""
 
     def __init__(self, client_instance):
-        self.__client = client_instance
+        self._client = client_instance
 
     @property
-    def _client(self):
+    def client(self):
         """An instance of the objectrocket.client.Client."""
-        if 'objectorcket.client' not in sys.modules:
-            from objectrocket import client
-        if not isinstance(self.__client, client.Client):
-            return None
-        return self.__client
+        return self._client
 
-    def _verify_auth(self, resp, *args, **kwargs):
-        """Verify that the response object did not receive a 401."""
-        if resp.status_code == 401:
-            raise errors.AuthFailure('Received response code 401 from {} {}. Keypair used: {}:{}'
-                                     ''.format(resp.request.method, resp.request.path_url,
-                                               self._client.user_key, self._client.pass_key))
+    def _verify_auth(self, *args, **kwargs):
+        """A wrapper around :py:meth:`objectrocket.client.Client._verify_auth`."""
+        self.client._verify_auth(*args, **kwargs)
