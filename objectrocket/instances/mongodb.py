69a6ea3a8dd853c90f9a04d2ef31924edf02268d
Keshav Magge <keshav@keshavmagge.com>
2016-05-19 17:04:56 -0500

    pull (sharded) mongo instance stats for all shards parallely using a thread pool

--- objectrocket/instances/mongodb.py
+++ objectrocket/instances/mongodb.py
@@ -2,9 +2,12 @@
 import datetime
 import json
 import logging
+import time
 
 import pymongo
 import requests
+
+from concurrent import futures
 
 from objectrocket import bases
 from objectrocket import util
@@ -98,6 +101,11 @@ class MongodbInstance(bases.BaseInstance, bases.Extensible, bases.InstanceAclsIn
 
         return response.json()
 
+
+    def get_aggregate_database_stats(self):
+        return requests.get(self._service_url + 'aggregate_database_stats/',
+                            **self._instances._default_request_kwargs).json()['data']
+
     @property
     @util.token_auto_auth
     def new_relic_stats(self):
@@ -105,12 +113,89 @@ class MongodbInstance(bases.BaseInstance, bases.Extensible, bases.InstanceAclsIn
         Get stats for this instance.
         """
         if self._new_relic_stats is None:
-            response = requests.get('{}{}'.format(self._url,
-                                    'new-relic-stats'),
-                                    **self._instances._default_request_kwargs)
-            self._new_relic_stats = json.loads(response.content).get(
-                'data') if response.status_code == 200 else {}
+            # if this is a sharded instance, fetch shard stats in parallel
+            if self.type == 'mongodb_sharded':
+                shards = [Shard(self.name, self._service_url + 'shards/',
+                                self._client, shard_doc)
+                          for shard_doc in self.shards().get('data')]
+                fs = []
+                with futures.ThreadPoolExecutor(len(shards)) as executor:
+                    for shard in shards:
+                        fs.append(executor.submit(shard.get_shard_stats))
+                    futures.wait(fs, timeout=None, return_when=futures.ALL_COMPLETED)
+                stats_this_second = self._rollup_shard_stats_to_instance_stats(
+                    {shard.name: future.result() for (shard, future) in zip(shards, fs)})
+                # power nap
+                time.sleep(1)
+                # fetch again
+                with futures.ThreadPoolExecutor(len(shards)) as executor:
+                    for shard in shards:
+                        fs.append(executor.submit(shard.get_shard_stats))
+                    futures.wait(fs, timeout=None, return_when=futures.ALL_COMPLETED)
+                stats_next_second = self._rollup_shard_stats_to_instance_stats(
+                    {shard.name: future.result() for (shard, future) in zip(shards, fs)})
+                self._new_relic_stats = self._compile_new_relic_stats(stats_this_second, stats_next_second)
+            else:
+                # fetch stats like we did before (by hitting new_relic_stats API resource)
+                response = requests.get('{}{}'.format(self._url,
+                                        'new-relic-stats'),
+                                        **self._instances._default_request_kwargs)
+                self._new_relic_stats = json.loads(response.content).get(
+                    'data') if response.status_code == 200 else {}
         return self._new_relic_stats
+
+    def _rollup_shard_stats_to_instance_stats(self, shard_stats):
+        """
+
+        :param shard_stats: dict of {shard_name: shard level stats}
+        :return: roll up all shard stats to instance level
+        """
+        instance_stats = {}
+        opcounters_per_node = []
+
+        # aggregate replication_lag
+        instance_stats['replication_lag'] = max(map(lambda s: s['replication_lag'], shard_stats.values()))
+
+        aggregate_server_statistics = {}
+        for shard_name, stats in shard_stats.items():
+            for statistic_key in stats.get('shard_stats'):
+                if statistic_key != 'connections' and statistic_key in aggregate_server_statistics:
+                    aggregate_server_statistics[statistic_key] = util.sum_values(aggregate_server_statistics[statistic_key],
+                                                                                 stats.get('shard_stats')[statistic_key])
+                else:
+                    aggregate_server_statistics[statistic_key] = stats.get('shard_stats')[statistic_key]
+
+            # aggregate per_node_stats into opcounters_per_node
+            opcounters_per_node.append({shard_name: {member: node_stats['opcounters']
+                                                     for member, node_stats in stats.get('per_node_stats').items()}})
+
+        instance_stats['opcounters_per_node'] = opcounters_per_node
+        instance_stats['aggregate_server_statistics'] = aggregate_server_statistics
+        return instance_stats
+
+    def _compile_new_relic_stats(self, stats_this_second, stats_next_second):
+        server_statistics_per_second = {}
+        opcounters_per_node_per_second = []
+        for subdoc in ["opcounters", "network"]:
+            first_doc = stats_this_second['aggregate_server_statistics'][subdoc]
+            second_doc = stats_next_second['aggregate_server_statistics'][subdoc]
+            keys = set(first_doc.keys()) | set(second_doc.keys())
+            server_statistics_per_second[subdoc] = {key: int(second_doc[key]) - int(first_doc[key]) for key in keys}
+        for node1, node2 in zip(stats_this_second['opcounters_per_node'], stats_next_second['opcounters_per_node']):
+            node_opcounters_per_second = {}
+            for repl, members in node2.items():
+                node_opcounters_per_second[repl] = {}
+                for member, ops in members.items():
+                    node_opcounters_per_second[repl][member] = {}
+                    for op, count in ops.items():
+                        node_opcounters_per_second[repl][member][op] = count - node1[repl][member][op]
+            opcounters_per_node_per_second.append(node_opcounters_per_second)
+
+        return {'opcounters_per_node_per_second': opcounters_per_node_per_second,
+                'server_statistics_per_second': server_statistics_per_second,
+                'aggregate_server_statistics': stats_next_second.get('aggregate_server_statistics'),
+                'replication_lag': stats_next_second.get('replication_lag'),
+                'aggregate_database_statistics': self.get_aggregate_database_stats()}
 
 
     @property
@@ -174,3 +259,43 @@ class MongodbInstance(bases.BaseInstance, bases.Extensible, bases.InstanceAclsIn
             connect_string = self.ssl_connect_string
 
         return pymongo.MongoClient(connect_string)
+
+
+class Shard(bases.Extensible):
+    def __init__(self, instance_name, stats_base_url, or_client, shard_document):
+        self._instance_name = instance_name
+        self._shardstr = shard_document['shardstr']
+        self._plan = shard_document['plan']
+        self._id = shard_document['id']
+        self._name = shard_document['name']
+        self._stats_base_url = stats_base_url
+        self._client = or_client
+
+    @property
+    def instance_name(self):
+        return self._instance_name
+
+    @property
+    def shard_string(self):
+        return self._shardstr
+
+    @property
+    def plan(self):
+        return self._plan
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def id(self):
+        return self._id
+
+    def get_shard_stats(self):
+        return requests.get(self._stats_url, params={'include_stats': True},
+                                   headers={'X-Auth-Token': self._client.auth._token}
+                                   ).json()['data']['stats']
+
+    @property
+    def _stats_url(self):
+        return '%s%s/' % (self._stats_base_url, self.name)
